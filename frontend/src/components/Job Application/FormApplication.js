ccfafe95a16e3ba9060a914b463e77aaed4e8ac6
SandaruwanChandrasena <schandrasena98@gmail.com>
2024-11-19 13:11:55 +0530

    ITP Project -Final Commit

---
+++ frontend/src/components/Job Application/FormApplication.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './FormApplication.css'; // Updated CSS file name
+
+
+const FormApplication = (props) => {
+    const { label, errorMessage, onChange, ...inputProps } = props;
+    
+    const [touched, setTouched] = useState(false);
+    const [error, setError] = useState(false);
+  
+    const handleInputChange = (e) => {
+      onChange(e); // Propagate the change event
+      setError(!e.target.validity.valid); // Update error state based on validity
+    };
+  
+    const handleBlur = () => {
+      setTouched(true); // Mark the input as touched when it's blurred
+    };
+  
+    return (
+      <div className="job-application-form"> {/* Updated class name */}
+        <label>{label}</label>
+        {inputProps.type === 'select' ? (
+                <select
+                    {...inputProps}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur} // Triggered when the input is blurred
+                >
+                    {inputProps.options.map(option => (
+                        <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+            ) : (
+                <input
+                    {...inputProps}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur} // Triggered when the input is blurred
+                />
+            )}
+            {touched && error && <span>{errorMessage}</span>} {/* Display error message when input is touched and invalid */}
+        </div>
+    );
+};
+
+export default FormApplication;
